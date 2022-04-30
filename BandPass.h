0a99dbde5f5832e6a33143c62b8402a883a2dc53
Luke Coles <12luke04@gmail.com>
2022-04-30 18:09:17 +0100

    push

--- BandPass.h
+++ BandPass.h
@@ -2,23 +2,31 @@
 #define BANDPASS_H
 
 #include <algorithm>
+#include "LowPass.h"
+#include "HighPass.h"
 
 class BandPass {
     public:
-        BandPass(float fcl_Hz, float fch_Hz, float fs_Hz, float gain) {
+        BandPass(float fcl_Hz, float fch_Hz, float fs_Hz, float gain) : _lpFilt(fch_Hz, fs_Hz, 1.0f), _hpFilt(fcl_Hz, fs_Hz, 1.0f) {
             _fs_Hz = fs_Hz;
             set_cutoff(fcl_Hz, fch_Hz);
             set_gain(gain);
             _output = 0.0f;
             _inputLast = 0.0f;
+
         }
         float update(float input) {
+            /*
             _lowOut = _coeffLP[0] * input + _coeffLP[1] * _lowOut;  
             _output = _coeffHP * (_lowOut - _inputLast + _output);
             _inputLast = _lowOut;
             return _output * _gain;
+            */
+            float lowOut = _lpFilt.update(input);
+            return _hpFilt.update(lowOut);
         }
         void set_cutoff(float fcl_Hz, float fch_Hz) {
+            /*
             fch_Hz = std::min(fch_Hz, (0.5f * _fs_Hz));
             fch_Hz = std::max(fch_Hz, 0.0f);
             float alpha = (6.28318530718f * fch_Hz) / _fs_Hz;
@@ -29,17 +37,25 @@ class BandPass {
             fcl_Hz = std::max(fcl_Hz, 0.0f);
             alpha = (6.28318530718f * fcl_Hz) / _fs_Hz;
             _coeffHP = 1.0f / (1.0f + alpha);
+            */
+
+            _lpFilt.set_cutoff(fch_Hz);
+            _hpFilt.set_cutoff(fcl_Hz);
         }
-        void set_gain(float gain) {_gain = gain;}
+        void set_gain(float gain) {
+            _gain = gain;
+        }
 
     private:
         float _inputLast;
         float _output;
-        float _lowOut;
         float _fs_Hz;
         float _coeffLP[2];
         float _coeffHP;
         float _gain;
+
+        LowPass _lpFilt;
+        HighPass _hpFilt;
 };
 
 
--- main.cpp
+++ main.cpp
@@ -8,19 +8,34 @@
 #include <cstdint>
 #include <cstdlib>
 
+#define PIN_INPUT      PC_0
+#define PIN_OUTPUT     PA_4
+#define PIN_INDICATOR  PA_5
+#define PIN_LOW_GAIN   PA_1
+#define PIN_MID_GAIN   PA_7
+#define PIN_HIGH_GAIN  PB_0
+#define PIN_LOW_FREQ   PA_0
+#define PIN_HIGH_FREQ  PC_1
+
+#define LOW_FREQ_MIN   20.0f
+#define LOW_FREQ_MAX   500.0f
+#define HIGH_FREQ_MIN  1000.0f
+#define HIGH_FREQ_MAX  10000.0f
+
 static BufferedSerial pc(USBTX, USBRX);
 
-AnalogIn inputSig(PC_0);
-AnalogOut  outputSig(PA_4);
+AnalogIn inputSig(PIN_INPUT);
+AnalogOut  outputSig(PIN_OUTPUT);
 float input;
 float output;
 
-DigitalOut indicator(PA_5);
+DigitalOut indicator(PIN_INDICATOR);
 
-AnalogIn lowAmplitude_in(PA_0);
-AnalogIn lowFrequency_in(PA_1);
-AnalogIn highAmplitude_in(PB_0);
-AnalogIn highFrequency_in(PC_1);
+AnalogIn lowAmplitude_in(PIN_LOW_GAIN);
+AnalogIn lowFrequency_in(PIN_LOW_FREQ);
+AnalogIn highAmplitude_in(PIN_HIGH_GAIN);
+AnalogIn highFrequency_in(PIN_HIGH_FREQ);
+AnalogIn midAmplitude_in(PIN_MID_GAIN);
 
 const float offset = 0.3030303f; 
 const float pi     = 3.14159265358979;
@@ -31,7 +46,7 @@ float sampleFreq = 48000.0f;
 int sampleInterval_us(int(1000000/sampleFreq));
 int sampleFreq_actual(1000000/sampleInterval_us);
 
-int controlFreq(1000);
+int controlFreq = 10000;
 
 float highFreq = 150.0f;
 
@@ -42,6 +57,8 @@ BandPass bpFilt(lowFreq, highFreq, sampleFreq_actual, 1.0f);
 Timer t;
 
 void process_eq();
+void update_controls();
+float scale(float in, float in_min, float in_max, float out_min, float out_max);
 
 int main()
 {  
@@ -65,8 +82,7 @@ int main()
             }
 
             if(ticks % controlFreq == 0) {
-                lpFilt.set_gain(lowAmplitude_in.read());
-                hpFilt.set_gain(highAmplitude_in.read());
+                update_controls();
             }
         }
         
@@ -78,4 +94,27 @@ void process_eq() {
     //output = bpFilt.update(input);
     output = hpFilt.update(input) + lpFilt.update(input) + bpFilt.update(input);
     outputSig.write(output + offset);
+}
+
+void update_controls() {
+    lpFilt.set_gain(lowAmplitude_in.read());
+    hpFilt.set_gain(highAmplitude_in.read());
+    bpFilt.set_gain(midAmplitude_in.read());
+
+    int lowCut = scale(lowFrequency_in.read(), 0.0f, 1.0f, LOW_FREQ_MIN, LOW_FREQ_MAX);
+    int highCut = scale(highFrequency_in.read(), 0.0f, 1.0f, HIGH_FREQ_MIN, HIGH_FREQ_MAX);
+
+    lpFilt.set_cutoff(lowCut);
+    hpFilt.set_cutoff(highCut);
+    bpFilt.set_cutoff(lowCut, highCut);
+/*
+    printf("\n\n\n\n\n\n\n\n\n");
+    printf("Low Cut   = %i\n", lowCut);
+    printf("High Cut  = %i\n", highCut);
+*/
+}
+
+float scale(float in, float in_min, float in_max, float out_min, float out_max) {
+    float in_percent = in_min / (in / (in_max - in_min));
+    return (in_percent * (out_max - out_min)) + out_min;
 }
